b3b1284b8e85521bc69cc83d4b00cd38830b8b86
Robert Kolar <robert.kolar@highpowerbear.com>
2018-10-27 06:55:03 +0200

    Add additional contract multipliers

--- core/src/main/java/com/highpowerbear/hpbanalytics/enums/ContractMultiplier.java
+++ core/src/main/java/com/highpowerbear/hpbanalytics/enums/ContractMultiplier.java
@@ -6,24 +6,25 @@ package com.highpowerbear.hpbanalytics.enums;
 public enum ContractMultiplier {
     ES(50),
     NQ(20),
+    RTY(50),
     YM(5),
-    GC(100),
-    ZB(1000),
-    M6E(12500),
-    M6A(10000),
-    M6B(6250),
     ESTX50(10),
     DAX(5),
     SMI(10),
     N225(1000),
     N225M(100),
-    K200(250000),
-    K200M(50000),
     HSI(50),
     MHI(10),
     HHI_HK(50),
-    MCH_HK(10);
-
+    MCH_HK(10),
+    K200(250000),
+    K200M(50000),
+    ZB(1000),
+    GC(100),
+    CL(1000),
+    M6E(12500),
+    M6A(10000),
+    M6B(6250);
 
     private final Integer multiplier;
 
